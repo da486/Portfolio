2c22d2afd858358a47141013830fe5cba00fc923
da486 <eon486da@gmail.com>
2021-10-09 23:54:13 +0900

    Start project

---
+++ js/data.js
@@ -0,0 +1,87 @@
+var my_info = {
+    id: 'da486',
+    user_name: "강다겸",
+    introduction: "사진 보는 걸 좋아하는 프로그래머 지망생입니다.",
+    as: "hobby",
+    interest: ["nature", "thing"],
+    like: [2, 5, 9],
+    follow: ['photo-luv', 'pixa', 'googler', 'js-master']
+  }
+  
+  var photos = [
+    {
+      idx: 1,
+      user_id: 'js-master',
+      user_name: '연놀부',
+      file_name: 'photo_1.jpg',
+      description: '새로 한 벽지',
+      likes: 1,
+    },
+    {
+      idx: 2,
+      user_id: 'googler',
+      user_name: '심청',
+      file_name: 'photo_2.jpg',
+      description: '뒷마당 나무랑 뒷동산',
+      likes: 3,
+    },
+    {
+      idx: 3,
+      user_id: 'mozilla',
+      user_name: '김삿갓',
+      file_name: 'photo_3.jpg',
+      description: '수질 대박',
+      likes: 2,
+    },
+    {
+      idx: 4,
+      user_id: 'hackerman',
+      user_name: '콩쥐',
+      file_name: 'photo_4.jpg',
+      description: '다음 휴가는 여기로',
+      likes: 5,
+    },
+    {
+      idx: 5,
+      user_id: 'pixa',
+      user_name: '연흥부',
+      file_name: 'photo_5.jpg',
+      description: '나를 집사로 간택한 고양이',
+      likes: 9,
+    },
+    {
+      idx: 6,
+      user_id: 'com-al-mot',
+      user_name: '성춘향',
+      file_name: 'photo_6.jpg',
+      description: '두시간 반만에 성공했어요!',
+      likes: 4,
+    },
+    {
+      idx: 7,
+      user_id: 'photo-luv',
+      user_name: '임꺽정',
+      file_name: 'photo_7.jpg',
+      description: '해바라기 같은 사랑',
+      likes: 7,
+    },
+    {
+      idx: 8,
+      user_id: 'star486',
+      user_name: 'quf',
+      file_name: 'photo_8.jpg',
+      description: '강아지랑 뒷산에 오르다',
+      likes: 2,
+    },
+    {
+      idx: 9,
+      user_id: 'coder',
+      user_name: '홍길동',
+      file_name: 'photo_9.jpg',
+      description: '여행중 발견한 풍경',
+      likes: 4,
+    }
+  ]
+
+
+
